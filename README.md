b04e626179b0a9c3bc2e0beed3df5042f5db38aa
Don Gumayagay <dngumayagay@gmail.com>
2022-07-11 15:45:30 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+to run dev
+
+in svelte directory run
+
+```
+npm run dev
+```
+
+then in root directory run
+
+```
+neu run --frontend-lib-dev
+```
+
+to build run
+
+in svelte diretory run
+
+```
+npm run build
+```
+
+then in root directory run
+
+```
+neu build
+```
---
+++ my-app-svelte/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  // @ts-nocheck
+
+  Neutralino.init();
+</script>
+
+<main>
+  <!-- svelte-ignore missing-declaration -->
+  <button
+    on:click={async () => {
+      await Neutralino.app.exit();
+    }}>quit</button
+  >
+  <!-- svelte-ignore missing-declaration -->
+  <button
+    on:click={async () => {
+      await Neutralino.filesystem.writeFile("./myFile.txt", "Sample content");
+    }}>create file</button
+  >
+</main>
